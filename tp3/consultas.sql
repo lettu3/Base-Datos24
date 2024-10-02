536b49230232de4c20cb695e8a2833d78b0196dc
Ignacio Gomez Barrios <117108736+lettu3@users.noreply.github.com>
2024-10-02 15:22:15 -0300

    Update consultas.sql

--- tp3/consultas.sql
+++ tp3/consultas.sql
@@ -29,4 +29,72 @@ WHERE countrylanguage.Percentage > (SELECT MAX(Percentage)
 								    WHERE countrylanguage.CountryCode = country.Code
 								    AND countrylanguage.IsOfficial = 'T');
 
+-- Ejercicio 5
+-- con subquery
+SELECT country.Region FROM country
+WHERE country.SurfaceArea < 1000 
+AND country.Code IN (
+	SELECT city.CountryCode
+	FROM city
+	WHERE city.Population > 100000
+);
 
+
+-- sin subquery
+SELECT country.Region FROM country
+INNER JOIN city ON city.CountryCode = country.Code
+WHERE country.SurfaceArea < 1000 AND city.Population > 100000;
+
+
+-- Ejercicio 6
+-- con consulta escalar 
+SELECT country.Name, city.Population FROM country
+INNER JOIN city ON city.CountryCode = country.Code
+WHERE city.population = (
+	SELECT max(city.Population)
+	FROM city 
+	WHERE city.CountryCode = country.Code
+);
+
+-- con agregacion (MAX)
+SELECT country.Name, MAX(city.Population) FROM country
+INNER JOIN city ON city.CountryCode = country.Code
+GROUP BY country.Name;
+
+
+-- Ejercicio 7
+select country.Name, countrylanguage.language
+from country
+inner join countrylanguage on country.Code = countrylanguage.CountryCode
+where countrylanguage.Percentage > (
+	select avg(countrylanguage.Percentage)
+	from countrylanguage
+	where countrylanguage.CountryCode = country.Code and countrylanguage.IsOfficial = 'T'
+);
+
+-- Ejercicio 8
+select continent.Name, sum(country.Population) as TotalPopulation
+from continent
+inner join country on country.Continent = continent.Name
+group by continent.Name
+order by TotalPopulation desc;
+
+
+-- Ejercicio 9
+select continent.Name, avg(country.LifeExpectancy) as LifeExpectancy
+from continent
+inner join country on country.Continent = continent.Name
+where LifeExpectancy > 40 and LifeExpectancy < 70
+group by continent.Name
+
+	
+-- Ejercicio 10
+
+select continent.Name,
+		max(country.Population) as MaxPopulation,
+		min(country.Population) as MinPopulation,
+		avg(country.Population) as AvgPopulation,
+		sum(country.Population) as TotalPopulation
+from continent
+inner join country on continent.Name = country.Continent
+group by continent.Name;
