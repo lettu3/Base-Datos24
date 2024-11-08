189e8919cf72886e4e8053aa3eb2af0fac3c956d
Ignacio Gomez Barrios <117108736+lettu3@users.noreply.github.com>
2024-11-08 10:16:50 -0300

    Update mongodocker.sh

--- tp7/mongodocker.sh
+++ tp7/mongodocker.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-#pullear la imagen mas reciente de mongo
+# Pullear la imagen mas reciente de mongo
 docker pull mongo
 
-#correr el contenedor, con nombre $NAME, y directorio host $DIRECTORY, en el puerto 27017
+# Correr el contenedor, con nombre $NAME, y directorio host $DIRECTORY, en el puerto 27017
 docker run -d --name $(NAME) -p 27017:27017 -v $(DIRECTORY):/data/db mongo:latest
 
-#verificacion de que efectivamente el contenedor esta corriendo
+# Verificacion de que efectivamente el contenedor esta corriendo
 docker ps
 
-#printea la ip en la que esta corriendo el contenedor
+# Printea la ip en la que esta corriendo el contenedor
 docker inspect -f "{{ .NetworkSettings.IPAddress }}" $(NAME)
 
-#para poder copiar archivos dentro del contenedor
+# Para poder copiar archivos dentro del contenedor
 docker cp $(FILE_TO_COPY) $(NAME):$(ROUTE_IN_CONTAINER)
 
-#para poder realizar comandos desde el contenedor
+# Para poder realizar comandos desde el contenedor
 docker exec -it $(NAME) $(COMMAND)
+
+# Para poder conectarse desde la extension de VSCode mongodb://127.0.0.1:27017/$(NAME)
+
